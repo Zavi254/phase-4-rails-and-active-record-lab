0c51093f364de3a0fde42ec20aa8a200fb6652d9
Steven Otianga <steveotianga8991@gmail.com>
2022-09-27 02:52:19 +0300

    commit tree 6008dd96fc0b4fe518334291ce1cb3a052635d6c

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name , grade:Faker::Number.between(from: 18, to: 24) )
